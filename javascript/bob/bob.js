28776ff295e5c2aaf41a8d854b83e5b815db61e4
Jan Sziegaud <j.sziegaud@gmail.com>
2017-09-13 20:31:13 +0200

    iteration javascript/bob

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,17 +1,14 @@
 var Bob = function () {}
 
 Bob.prototype.hey = function (input) {
-  var numbers = input.match(/\d/)
-  var specialchars = input.match(/,/g)
-  var check = input.replace(/,/g, '')
-    .replace(/\d/g, '')
-    .replace(/\s/g, '')
-  if (specialchars && numbers && !check) return 'Whatever.'
-  if (!check && !numbers) return 'Fine. Be that way!'
-  if (check.length === 1 && check[0] === '?') return 'Sure.'
-  if (check === check.toUpperCase()) return ('Whoa, chill out!')
-  if (check.charAt(check.length - 1) === '?') return 'Sure.'
-  return 'Whatever.'
+  if (isEmpty(input)) return "Fine. Be that way!"
+  if (isQuestion(input)) return "Sure."
+  if (isYelling(input)) return "Whoa, chill out!" 
+  return "Whatever."
 }
+
+const isEmpty = input => input.trim() === ""
+const isQuestion = input => input.endsWith('?')
+const isYelling = input => (input.toUpperCase() === input) && (input.toLowerCase() !== input)
 
 module.exports = Bob
--- javascript/bob/bob.spec.js
+++ javascript/bob/bob.spec.js
@@ -65,7 +65,7 @@ describe('Bob', function() {
     expect(result).toEqual('Whoa, chill out!');
   });
 
-  xit('calmly speaking about umlauts', function() {
+  it('calmly speaking about umlauts', function() {
     var result = bob.hey('\xfcML\xe4\xdcTS');
     expect(result).toEqual('Whatever.');
   });
@@ -90,7 +90,7 @@ describe('Bob', function() {
     expect(result).toEqual('Fine. Be that way!');
   });
 
-   it('prolonged silence', function () {
+  it('prolonged silence', function () {
     var result = bob.hey('   ');
     expect(result).toEqual('Fine. Be that way!');
   });
