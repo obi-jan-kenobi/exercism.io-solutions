d2bc744fff3c11ae2aecce8cd732711507bbede7
Jan Sziegaud <j.sziegaud@gmail.com>
2017-09-20 20:37:25 +0200

    haskell/acronym

--- haskell/acronym/src/Acronym.hs
+++ haskell/acronym/src/Acronym.hs
@@ -3,14 +3,18 @@ module Acronym (abbreviate) where
 import Data.Char (toUpper, isUpper, isAlpha, isSpace)
 
 abbreviate :: String -> String
-abbreviate = firstAndUppers . words . filter isAlpha
+abbreviate = firstAndUppers . words . map hyphenToSpace
 
 firstAndUppers :: [String] -> String
-firstAndUppers = concat . fmap (uppers . upperFirst) 
+firstAndUppers = concat . fmap (uppers . upperFirst . filter (\x -> isAlpha x || isSpace x)) 
 
 uppers :: String -> String
 uppers [] = []
 uppers word = if all isUpper word then [head word] else filter isUpper word
+
+hyphenToSpace :: Char -> Char
+hyphenToSpace '-' = ' '
+hyphenToSpace c = c
 
 upperFirst :: String -> String
 upperFirst [] = []
