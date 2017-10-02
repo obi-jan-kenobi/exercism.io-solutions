823af2731ef75bc7f77b50baa4f4778832778741
Jan Sziegaud <j.sziegaud@gmail.com>
2017-10-02 09:49:08 +0200

    rust/gigasecond

---
+++ rust/gigasecond/src/lib.rs
@@ -0,0 +1,9 @@
+extern crate chrono;
+use std::ops::Add;
+use chrono::*;
+
+
+// Returns a Utc DateTime one billion seconds after start.
+pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
+    start.add(Duration::seconds(1000000000))
+}
