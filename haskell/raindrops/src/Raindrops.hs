5f2c536b11832adb6db093198b6eee38043c9ff1
Jan Sziegaud <j.sziegaud@gmail.com>
2018-04-18 22:28:09 +0200

    haskell/strain

--- haskell/raindrops/src/Raindrops.hs
+++ haskell/raindrops/src/Raindrops.hs
@@ -4,8 +4,10 @@ convert :: Int -> String
 convert n = let raindrops = [ x | x <- [3,5,7], isFactor x n] >>= toRaindrop
     in if null raindrops then show n else raindrops
 
-isFactor n x = x `mod` n == 0
+isFactor :: Int -> Int -> Bool
+isFactor x n = n `mod` x == 0
 
+toRaindrop :: Int -> String
 toRaindrop x
     | x == 3 = "Pling"
     | x == 5 = "Plang"
